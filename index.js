801b66ddbccfd3d6db9a18b4b497f5bc4a4c6b0b
Younis Rahman <younisrahman007@gmail.com>
2022-11-24 03:07:00 +0600

    test complete

--- index.js
+++ index.js
@@ -1,36 +1,61 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-//👇🏻 New imports
 const http = require('http').Server(app);
 const cors = require('cors');
-
+const PORT = 4000;
 const socketIO = require('socket.io')(http, {
   cors: {
-    origin: '<http://localhost:3000>',
+    origin: 'http://192.168.31.189:3000',
   },
 });
 
-//👇🏻 Add this before the app.get() block
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+const generateID = () => Math.random().toString(36).substring(2, 10);
+let chatRooms = [];
+
 socketIO.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
+  socket.on('createRoom', (name) => {
+    socket.join(name);
+    chatRooms.unshift({ id: generateID(), name, messages: [] });
+    socket.emit('roomsList', chatRooms);
+  });
+
+  socket.on('findRoom', (id) => {
+    let result = chatRooms.filter((room) => room.id == id);
+    // console.log(chatRooms);
+    socket.emit('foundRoom', result[0].messages);
+    // console.log("Messages Form", result[0].messages);
+  });
+
+  socket.on('newMessage', (data) => {
+    const { room_id, message, user, timestamp } = data;
+    let result = chatRooms.filter((room) => room.id == room_id);
+    const newMessage = {
+      id: generateID(),
+      text: message,
+      user,
+      time: `${timestamp.hour}:${timestamp.mins}`,
+    };
+    console.log('New Message', newMessage);
+    socket.to(result[0].name).emit('roomMessage', newMessage);
+    result[0].messages.push(newMessage);
+
+    socket.emit('roomsList', chatRooms);
+    socket.emit('foundRoom', result[0].messages);
+  });
   socket.on('disconnect', () => {
     socket.disconnect();
     console.log('🔥: A user disconnected');
   });
 });
 
-app.use(cors());
-
 app.get('/api', (req, res) => {
-  res.json({
-    message: 'Hello world',
-  });
+  res.json(chatRooms);
 });
 
 http.listen(PORT, () => {
